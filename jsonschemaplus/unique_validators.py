a9314baad3772e93b321a614bed47497fa827a31
Marc Wilson <marcw@silverfern.io>
2016-11-06 21:10:19 +0000

    Remove jsonschemaplus/*_validators.py

--- jsonschemaplus/unique_validators.py
+++
@@ -1,7 +0,0 @@
-def unique(array):
-	for i in range(len(array) - 1):
-		for j in range(i + 1, len(array)):
-			if (array[i] == array[j] and
-				type(array[i]) == type(array[j])):
-				return False
-	return True
